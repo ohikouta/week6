1d8d1c988ed74bc0c8e55f73600a39485f15604c
Kota Oi <kota.oi@serverworks.co.jp>
2025-05-13 21:54:32 +0900

    edit6

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App" style={{ textAlign: 'center', paddingTop: '50px' }}>
       <h1>くじ引きアプリ</h1>
-      <p>GithubActionsで自動デプロイ設定2</p>
+      <p>GithubActionsで自動デプロイ設定3</p>
       {result ? (
         <>
           <h2>結果: {result} 🎉</h2>
